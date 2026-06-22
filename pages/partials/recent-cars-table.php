5e3525895714a85bc0cf94bf16ccdb3f22e9563d
Pranavan Sivakumar <pranavansivakumar6@gmail.com>
2026-06-22 22:52:00 +1000

    Show sold price in recent cars

--- pages/partials/recent-cars-table.php
+++ pages/partials/recent-cars-table.php
@@ -2,13 +2,14 @@
 $cars = $recentCars ?? $pdo->query("SELECT * FROM cars ORDER BY created_at DESC LIMIT 8")->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <table>
-    <tr><th>Car</th><th>Status</th><th>Purchase</th><th>Estimated Sale</th><th>Action</th></tr>
+    <tr><th>Car</th><th>Status</th><th>Purchase</th><th>Estimated Sale</th><th>Sold Price</th><th>Action</th></tr>
     <?php foreach ($cars as $car): ?>
     <tr>
         <td><?= htmlspecialchars($car['year'].' '.$car['make'].' '.$car['model']) ?></td>
         <td><span class="badge"><?= htmlspecialchars($car['status']) ?></span></td>
         <td>$<?= number_format($car['purchase_price'], 2) ?></td>
         <td>$<?= number_format($car['estimated_sale_price'], 2) ?></td>
+        <td><?= (float) $car['actual_sale_price'] > 0 ? '$'.number_format($car['actual_sale_price'], 2) : '-' ?></td>
         <td><a class="btn secondary" href="/carfliphq/pages/car-detail.php?id=<?= $car['id'] ?>">Open</a></td>
     </tr>
     <?php endforeach; ?>
